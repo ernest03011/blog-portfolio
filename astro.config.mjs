934b633d31559160f856aa3af3528c073f988e0d
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-09-21 00:47:18 -0400

    eslint: apply linting configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,24 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import mdx from "@astrojs/mdx";
-import icon from "astro-icon";
+
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
-
-import sitemap from "@astrojs/sitemap";
-
-
 export default defineConfig({
-  site: 'https://manueldjs.online',
+  site: "https://manueldjs.online",
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx(), // Icon integration requires specific setting when using server or hybrid 
+  integrations: [mdx(), // Icon integration requires specific setting when using server or hybrid
   // to avoid that all icons from the library (mdi is being used) are installed
   // https://www.astroicon.dev/reference/configuration/
-  icon({
-    iconDir: "src/assets/icons",
-  }), sitemap()],
+    icon({
+      iconDir: "src/assets/icons",
+    }), sitemap()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
