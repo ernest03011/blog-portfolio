373185f9fbc1f9e0aebda21128136067c8aa2913
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-09-24 18:16:13 -0400

    chore: add a file with the social link information

---
+++ src/data/social-links.ts
@@ -0,0 +1,12 @@
+type iconName = `mdi:${string}`;
+
+export type SocialLinks = {
+  href: string;
+  iconName: iconName;
+  srMessage: string;
+};
+
+export const SocialLinks: SocialLinks[] = [
+  { href: "https://www.linkedin.com/in/manueldjesus/", iconName: "mdi:linkedin", srMessage: "Go to my Linkedin profile" },
+  { href: "https://github.com/ernest03011", iconName: "mdi:github", srMessage: "Go to my GitHub repo" },
+];
