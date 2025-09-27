249191791a6a6f46adb5387382c0747a2ce92741
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-09-27 11:09:26 -0400

    chore: add a page utility to handle the extraction and checking of the pages title and description

---
+++ src/utils/page.ts
@@ -0,0 +1,10 @@
+import type { PageKey } from "@data/pages";
+
+import { Pages } from "@data/pages";
+
+function getPageKey(pathname: string): PageKey {
+  const key = pathname === "/" ? "home" : pathname.split("/")[1] as PageKey;
+  return key in Pages ? key : "home";
+};
+
+export { getPageKey };
