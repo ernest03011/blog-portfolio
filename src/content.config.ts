607f0162e569c566ccb28e4eec9fb57714b1cb6c
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2026-02-18 22:33:19 -0400

    fix: specify the content for the blog collection using loader instead of type content that came from older astro version which was causing to fail to load the content

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,9 @@
 import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 
 const blog = defineCollection({
-  type: "content",
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.mdx" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
