4dfa52886fa8712b7b89cdef78dd5e8e085f925e
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2026-03-05 01:24:26 -0400

    chore: create filter component to be used in the filter section to add certain filters

---
+++ src/components/Filter.tsx
@@ -0,0 +1,29 @@
+type Filter = {
+  type: "tag" | "category" | "sort";
+  value: string;
+  onChange: (param: string) => void;
+  listOfOptions: string[];
+};
+export default function Filter({ type, value, onChange, listOfOptions }: Filter) {
+  return (
+    <div>
+      <label htmlFor={type}>
+        {type}
+      </label>
+      <select
+        id={type}
+        value={value}
+        onChange={e => onChange(e.target.value)}
+      >
+        {
+          listOfOptions.map(value => (
+            <option key={value} value={value}>
+              {value}
+            </option>
+          ))
+        }
+
+      </select>
+    </div>
+  );
+}
