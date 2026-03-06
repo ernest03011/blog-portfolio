ddfd1559eaa24ed158b279dd83656ae846aa3240
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2026-03-06 17:51:52 -0400

    chore: update the handleChange method and add the props to the filter section, also add an empty list component to display if there are no posts passed

--- src/components/ArticleListContainer.tsx
+++ src/components/ArticleListContainer.tsx
@@ -1,6 +1,9 @@
 import type { CollectionEntry } from "astro:content";
-import FilterSection from "@components/FilterSection.tsx";
-import PostPreviewCard from "@components/PostPreviewCard.tsx";
+import type { ChangeEvent } from "react";
+import EmptyList from "@components/EmptyList";
+import FilterSection from "@components/FilterSection";
+import PostPreviewCard from "@components/PostPreviewCard";
+import { useState } from "react";
 
 type Post = CollectionEntry<"blog">["data"] & {
   slug: CollectionEntry<"blog">["id"];
@@ -11,12 +14,25 @@ type Props = {
 };
 
 export default function ArticleListContainer({ allPosts }: Props) {
+  const [filterBy, setFilterBy] = useState("");
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setFilterBy(event.target.value);
+  };
+
+  const filteredPosts
+    = filterBy === ""
+      ? allPosts
+      : allPosts.filter(post => post.category === filterBy);
+
   return (
     <section className="text-content">
-      <FilterSection />
-      {allPosts.map(post => (
-        <PostPreviewCard key={post.slug} post={post} variant="moderate" />
-      ))}
+      <FilterSection filterBy={filterBy} handleChange={handleChange} />
+
+      {filteredPosts.length <= 0
+        ? <EmptyList />
+        : filteredPosts.map(post => (
+            <PostPreviewCard key={post.slug} post={post} variant="moderate" />
+          ))}
     </section>
   );
 }
