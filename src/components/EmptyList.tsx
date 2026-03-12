f9a2de9bc5c3d8974b2a7deb5059090ea40d85a9
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2026-03-12 01:40:42 -0400

    chore: improve the spacing and the text size

--- src/components/EmptyList.tsx
+++ src/components/EmptyList.tsx
@@ -1,6 +1,6 @@
 export default function EmptyList() {
   return (
-    <div>
+    <div className="p-12 my-6 text-center text-2xl">
       <p>No articles available at the moment!</p>
     </div>
   );
