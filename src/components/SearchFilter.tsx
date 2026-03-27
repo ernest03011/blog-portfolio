3dc5b8e548dc7003b24d14f39f5ea5c975bcb478
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2026-03-27 19:01:29 -0400

    chore: create a SearchFilter component in order to be able to customize further any Search Filter component

---
+++ src/components/SearchFilter.tsx
@@ -0,0 +1,28 @@
+import type { ChangeEvent } from "react";
+
+type SearchFilterProps = {
+  label: string;
+  searchQuery: string;
+  handleSearchQuery: (event: ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
+};
+
+export default function SearchFilter({ label, searchQuery, handleSearchQuery, className }: SearchFilterProps) {
+  const baseStyles = "border hover:border-accent px-2";
+
+  return (
+    <div>
+      <span className="block">
+        {label}
+        :
+      </span>
+      <input
+        type="text"
+        placeholder={`Search ${label}`}
+        value={searchQuery}
+        onChange={e => handleSearchQuery(e)}
+        className={`${baseStyles} ${className}`}
+      />
+    </div>
+  );
+}
